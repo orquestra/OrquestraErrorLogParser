c1fc29e8ff8dd47511c3539127185602d333cda6
Leonardo Luzzatto <leonardo.luzzatto@cryo.com.br>
2014-07-03 12:21:23 -0300

    Correcao nome solucao e projeto.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OrquestraErrorLogParses")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("OrquestraErrorLogParser")]
+[assembly: AssemblyDescription("Consolida logs de erro em arquivo CSV.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OrquestraErrorLogParses")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCompany("Cryo Technologies Ltda")]
+[assembly: AssemblyProduct("OrquestraErrorLogParser")]
+[assembly: AssemblyCopyright("Copyright Cryo Technologies ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
